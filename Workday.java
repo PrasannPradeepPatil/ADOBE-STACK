af72a4acce68e7c226021bd80662c3623a3436a6
prasannpradp <prasannpradp@adobe.com>
2022-06-17 14:38:08 -0400

    changes

---
+++ Workday.java
@@ -0,0 +1,112 @@
+//JAVA
+/*
+1.JAVA
+2.OOPM CONCEPTS
+3.difference between a class and a structure?
+4. Akka Supervisor and JVM work process
+5.enact garbage collection in Java
+6.What is Serialization
+7.SingleTon
+8.Given set of objects and was told to assign relations between them how I see fit.
+
+
+
+ */
+
+//JS,WEB
+/*
+1.LOOK AT WEB
+2.SOAP, Rest standards,GRAPHQL
+3.What happens when you type a url in the browser
+
+*/
+
+//DBMS
+/*
+1.Look at SQL
+2.Database:
+  Recording workouts
+   car dealerships. What would this look like and what are some things you might want to include?
+3.UML
+  UML Zoo diagram
+  UML diagram of a filesystem
+
+
+ */
+
+
+//DSA
+/*
+1.LOOK AT DSA
+2.creating a function to find repeating digits between a range of integers. 
+3.Reverse an integer without using string operations.
+4.reverse a string without using reverse/split functions
+5. median of a sorted array
+6.bunch of string manipulation through implementing a custom comparator so that strings get ordered a certain way.
+7.BFS traverse of a 2D matrix with memoization to find the shortest weighted path.
+8.find the level of a binary tree
+9.Given a list of lists, find the smallest interval of numbers that includes a number from each list.
+  Given K sorted integer arrays filled on average with N elements, how do you find a minimal span of numbers 
+  that contain at least one number from each array (inclusive of the boundaries).
+10.Determine if given strings are anagrams of each other.
+11.Find the first non-repeating character in a string.
+12.given a String of [a-z] characters, find and return the first character that appears only once
+13.Given a String of [a-z, A-Z, 0-9], find length of the longest substring that satisfies the following rules: a substring cannot contain digits [0-9], a substring should contain at least one capital character [A-Z].
+     e.g. given the String "aqW9ertyz", the longest valid substring has length 3 ("aqW").
+*/
+
+//SYSTEM DESIGN
+/*
+1.LOOK AT SYSTEM DESIGN
+2.design a netflix clone
+3.Explain about distributed micro services?
+4.Explain how does services scale?
+5.How is performance monitoring done
+ */
+
+//OS
+/*
+1.Deadlock
+2.Threads
+3.Elevator algo
+
+ */
+
+//PROJECTS
+/*
+1.LOOK AT PROJECTS IN RESUME,LINKEDIN, FLAIRLABS
+2.AGILE APPROACH
+ */
+
+//BEHAVORIAL
+/*
+1.LOOK AT BEHAVORIAL
+2.Hardest project you worked on 
+3.Describe how you increased diversity in your previous company. 
+4.What is Workday?
+5.Where do you see yourself in 5 years?
+6.Tell me about time you had conflict with co worker
+*/
+
+//JD
+/*
+About the Team
+Workday's Analytics organization is responsible for Workday Prism Analytics, core analytics and big data services. 
+Workday’s analytics products span data acquisition, data preparation, data discovery, business intelligence, and augmented 
+analytics/machine learning and use many open-source technologies like Hadoop, Spark and handle massive amounts of data using
+distributed systems and fault-tolerant architecture.
+
+About the Role
+We are looking for an outstanding Software Development Engineer to join our Analytics Performance and Scalability Engineering team. 
+Performance Software Engineers are highly skilled engineers who debug, measure, analyze, and improve our Workday Big Data Analytics
+ applications and infrastructure by working closely with all our engineering teams throughout the release cycle. 
+If you bring your excellent programming skills and curiosity about performance, then this is the role for you
+
+About You
+Basic Qualifications (Career)
+5+ years of experience in Performance Engineering, including benchmarking large-scale systems & applications
+2+ years working with Distributed Systems, specifically understanding the internals of typical Big Data systems (Hadoop, Spark, Kubernetes, etc.)
+2+ years using Java, Scala or Go
+BS degree in Computer Science, Math, or related field
+
+*/
